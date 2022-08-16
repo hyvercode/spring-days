9143137f2f97fe81a103264862dbe35d608bb2c1
mohirwanh <mohirwanh@gmail.com>
2022-08-16 22:20:20 +0700

    initial projects

---
+++ src/main/java/com/hyvercode/springday/SpringdayApplication.java
@@ -0,0 +1,13 @@
+package com.hyvercode.springday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyvercode/springday/SpringdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyvercode.springday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
