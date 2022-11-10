61b6165c99ac28b7fecd4d00d3400d42407fb0e2
mohirwanh <mohirwanh@gmail.com>
2022-11-10 15:08:33 +0700

    Spring Days Email Using Dynamic Template

--- src/test/java/com/hyvercode/springday/controller/mail/service/EmailServiceTest.java
+++ src/test/java/com/hyvercode/springday/controller/mail/service/EmailServiceTest.java
@@ -3,25 +3,29 @@ package com.hyvercode.springday.controller.mail.service;
 import com.hyvercode.springday.mail.model.EmailProperties;
 import com.hyvercode.springday.mail.service.EmailService;
 import com.hyvercode.springday.model.request.EmailTemplateRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import javax.mail.Message;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @ExtendWith(MockitoExtension.class)
 class EmailServiceTest {
-  @InjectMocks
+  @Spy
   private EmailService emailService;
 
   @Mock
   JavaMailSender javaMailSender;
 
+  @Mock
   EmailProperties emailProperties;
 
   EmailTemplateRequest request;
@@ -31,7 +35,7 @@ class EmailServiceTest {
   public static final String SUBJECT = "Account Unlocked";
 
   @BeforeEach
-  public void setup() {
+  public void setup(){
     emailProperties = new EmailProperties();
     emailProperties.setHostname("smtp.gmail.com");
     emailProperties.setPort(456);
@@ -39,21 +43,21 @@ class EmailServiceTest {
     emailProperties.setPassword("TEST");
     emailProperties.setSender("TEST");
 
-    emailService = new EmailService(javaMailSender,emailProperties);
+    emailService = new EmailService(javaMailSender, emailProperties);
 
     request = new EmailTemplateRequest();
     request.setEmailCode("TEST");
     request.setSubject(SUBJECT);
     request.setRecipient(EMAIL_ADDRESS);
     request.setMsgBody(BODY);
+    request.setAttachment(null);
+    initMocks(this);
   }
 
   @Test
-  void testSendEmail() throws Exception {
-//    Mockito.doNothing().when(emailService).send(any(Message.class));
-//    Assertions.assertDoesNotThrow(() -> emailService.sendMail(request));
-//
-//    verify(emailService).send(any(Message.class));
+  void testSendEmailTemplate() throws Exception {
+    Mockito.doNothing().when(emailService).send(any(Message.class));
+    Assertions.assertDoesNotThrow(() -> emailService.sendEmailWithTemplate(BODY, EMAIL_ADDRESS, SUBJECT));
   }
 }
 
